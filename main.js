bfe15b438674c9cfb5f51f691e57041fc3a925bf
Anton Polyakov <butkamone@gmail.com>
2015-04-09 11:38:05 +0700

    Mailgun Initialization

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 Parse.initialize("vsqeYKxDyEoRReTnfrzRy8ix9SWcvpiiwlNLxEc1", "DtolEKfbrDkvzdKSn80xm7zX9HS9yHYhGF30YhML");
 
+var Mailgun = require('mailgun');
+Mailgun.initialize('kozmetium.co', 'key-f6b34411303a1be37cbfa45ec26b6663');
