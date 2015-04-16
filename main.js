283ae12c01607fd1f5330a1f86b6fb39dc3eefb9
Anton Polyakov <butkamone@gmail.com>
2015-04-16 10:44:19 +0700

    pin

--- main.js
+++ main.js
@@ -2,3 +2,22 @@ Parse.initialize("vsqeYKxDyEoRReTnfrzRy8ix9SWcvpiiwlNLxEc1", "DtolEKfbrDkvzdKSn8
 
 var Mailgun = require('mailgun');
 Mailgun.initialize('kozmetium.co', 'key-f6b34411303a1be37cbfa45ec26b6663');
+
+function subscribe() {
+	var email = document.getElementById("email").value;
+	
+	var Subscribers = Parse.Object.extend("Subscribers");
+	var subscribers = new Subscribers();
+	 
+	subscribers.save({
+	  email: email
+	}, {
+	  success: function(subscribers) {
+	    // The object was saved successfully.
+	  },
+	  error: function(subscribers, error) {
+	    // The save failed.
+	    // error is a Parse.Error with an error code and message.
+	  }
+	});
+}
